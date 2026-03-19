6a9a2e119a82204ce137ef6f39c4bef414447ca5
Uzair Ahmed <uzair.raoiqra101@gmail.com>
2026-03-20 00:03:00 +0500

    Update email and initials in Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -43,14 +43,14 @@ const Navbar = () => {
     <>
       <div className="header">
         <a href="/#" className="navbar-title" data-cursor="disable">
-          RC
+          UA
         </a>
         <a
-          href="mailto:rajeshchittyal21@gmail.com"
+          href="mailto:uzair.raoiqra101@gmail.com"
           className="navbar-connect"
           data-cursor="disable"
         >
-          rajeshchittyal21@gmail.com
+          uzair.raoiqra101@gmail.com
         </a>
         <ul>
           <li>
