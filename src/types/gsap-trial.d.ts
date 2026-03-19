dde401153a1617680b6b3eb5ee59602ec0bae1cc
Uzair Ahmed <uzair.raoiqra101@gmail.com>
2026-03-19 22:17:47 +0500

    Add separate type declaration file for gsap-trial

---
+++ src/types/gsap-trial.d.ts
@@ -0,0 +1,16 @@
+declare module "gsap-trial/SplitText" {
+  export class SplitText {
+    constructor(target: string | Element | (string | Element)[], config?: SplitTextConfig);
+    chars: Element[];
+    words: Element[];
+    lines: Element[];
+    revert(): void;
+  }
+
+  interface SplitTextConfig {
+    type?: string;
+    linesClass?: string;
+    wordsClass?: string;
+    charsClass?: string;
+  }
+}
