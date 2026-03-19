6b61c2d923d1cc91e8a7a89c3b4b986c295bad50
Uzair Ahmed <uzair.raoiqra101@gmail.com>
2026-03-19 22:11:40 +0500

    Add TypeScript declaration for gsap-trial/SplitText

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,18 @@
 /// <reference types="vite/client" />
+
+declare module "gsap-trial/SplitText" {
+  export class SplitText {
+    constructor(target: string | Element | Element[], config?: SplitTextConfig);
+    chars: Element[];
+    words: Element[];
+    lines: Element[];
+    revert(): void;
+  }
+
+  interface SplitTextConfig {
+    type?: string;
+    linesClass?: string;
+    wordsClass?: string;
+    charsClass?: string;
+  }
+}
