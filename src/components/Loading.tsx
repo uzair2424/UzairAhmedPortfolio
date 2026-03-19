c2607b02f6f9610502f0b9a12a7311e5ff39ac00
Uzair Ahmed <uzair.raoiqra101@gmail.com>
2026-03-20 00:04:10 +0500

    Update loader initials and marquee text

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -46,7 +46,7 @@ const Loading = ({ percent }: { percent: number }) => {
     <>
       <div className="loading-header">
         <a href="/#" className="loader-title" data-cursor="disable">
-          RC
+          UA
         </a>
         <div className={`loaderGame ${clicked && "loader-out"}`}>
           <div className="loaderGame-container">
@@ -62,8 +62,8 @@ const Loading = ({ percent }: { percent: number }) => {
       <div className="loading-screen">
         <div className="loading-marquee">
           <Marquee>
-            <span> Full Stack Developer</span> <span>Software Engineer</span>
-            <span> Full Stack Developer</span> <span>Software Engineer</span>
+            <span> Frontend Developer</span> <span>React & Next.js</span>
+            <span> Frontend Developer</span> <span>React & Next.js</span>
           </Marquee>
         </div>
         <div
